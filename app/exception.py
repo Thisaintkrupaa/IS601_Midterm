f531063f803907e55b1508009ca023ad7dee8e36
Thisaintkrupaa <krupiikaa@gmail.com>
2026-03-03 15:50:12 -0500

    created and coded exception.py

---
+++ app/exception.py
@@ -0,0 +1,17 @@
+########################
+# Exception Hierarchy  #
+########################
+class CalculatorError(Exception):
+    pass
+
+
+class ValidationError(CalculatorError):
+    pass
+
+
+class OperationError(CalculatorError):
+    pass
+
+
+class ConfigurationError(CalculatorError):
+    pass
